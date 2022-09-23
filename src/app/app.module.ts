bcc1cf8277119c5015dfbb97f01f34e6c2c7f72c
oadbWeb <i_trade@hotmail.com>
2022-09-23 12:46:55 -0300

    Primer boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
